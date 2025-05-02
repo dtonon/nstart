0f1fa20bb54ad58c1e58b9a0c63caabb0558838a
Sebastix <info@sebastix.nl>
2025-05-02 14:54:10 +0200

    replace static text with translation string of home.continue_button

--- src/routes/[lang]/+page.svelte
+++ src/routes/[lang]/+page.svelte
@@ -223,7 +223,7 @@
 									class="inline-flex items-center rounded bg-accent px-10 py-4 text-[1.8rem] text-white"
 									on:click={() => goto('/en/yourself')}
 								>
-									Let's Start <img
+                  {t('home.continue_button')} <img
 										src="/icons/arrow-right.svg"
 										alt="Icon"
 										class="ml-4 mr-2 h-7 w-7"
